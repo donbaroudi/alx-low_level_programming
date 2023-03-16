76a4b8be88827ef4fb9588ec9ebea6991c55e33a
BOULBAROUD <jamalboulbaroud92@gmail.com>
2023-03-16 13:35:41 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - this is the main function
- * Return: Always 0 (Success)
+ *
+ * Return: 0 when successfull
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	return (0);
 }
